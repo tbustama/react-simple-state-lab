ab74c9a84e5e6bfff3c5edb628661f14c388a8ab
Taylor Bustamante <taylorbustamante16@gmail.com>
2021-02-01 20:47:53 -0800

    Done.

--- src/Matrix.js
+++ src/Matrix.js
@@ -1,21 +1,142 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import Cell from "./Cell.js";
 export default class Matrix extends Component {
-  
   genRow = (vals) => {
-    return vals.map(val => <div className="cell"></div>) // replace me and render a cell component instead!
-  }
-  
+    return vals.map((val) => <Cell value={val} />); // replace me and render a cell component instead!
+  };
+
   genMatrix = () => {
-    return this.props.values.map(rowVals => <div className="row">{this.genRow(rowVals)}</div>)
-  }
-  
+    return this.props.values.map((rowVals) => (
+      <div className="row">{this.genRow(rowVals)}</div>
+    ));
+  };
+
   render() {
-    return (
-      <div id="matrix">
-        {this.genMatrix()}
-      </div>
-    )
+    return <div id="matrix">{this.genMatrix()}</div>;
   }
-  
 }
+
+Matrix.defaultProps = {
+  values: [
+    [
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+    ],
+    [
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+    ],
+    [
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+    ],
+    [
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+    ],
+    [
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+    ],
+    [
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+    ],
+    [
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+    ],
+    [
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+    ],
+    [
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+    ],
+    [
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+      "#F00",
+    ],
+  ],
+};
